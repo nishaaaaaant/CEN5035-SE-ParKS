723f590ca33b5ecb6131882d5befe7ae97734de9
neeravjain24 <47077857+neeravjain24@users.noreply.github.com>
2022-02-02 21:20:04 -0500

    OAuth

--- parks/src/App.js
+++ parks/src/App.js
@@ -1,72 +1,25 @@
-import React from "react";
-import "./App.scss";
-import { Login, Register } from "./components/login/index";
+import React, { Component } from "react";
+import GoogleLogin from "react-google-login";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLogginActive: true
-    };
-  }
-
-  componentDidMount() {
-    //Add .right by default
-    this.rightSide.classList.add("right");
-  }
-
-  changeState() {
-    const { isLogginActive } = this.state;
-
-    if (isLogginActive) {
-      this.rightSide.classList.remove("right");
-      this.rightSide.classList.add("left");
-    } else {
-      this.rightSide.classList.remove("left");
-      this.rightSide.classList.add("right");
-    }
-    this.setState(prevState => ({ isLogginActive: !prevState.isLogginActive }));
-  }
+export class App extends Component {
+  responseGoogle = (response) => {
+    console.log(response);
+    console.log(response.profileObj);
+  };
 
   render() {
-    const { isLogginActive } = this.state;
-    const current = isLogginActive ? "Register" : "Login";
-    const currentActive = isLogginActive ? "login" : "register";
     return (
-      <div className="App">
-        <div className="login">
-          <div className="container" ref={ref => (this.container = ref)}>
-            {isLogginActive && (
-              <Login containerRef={ref => (this.current = ref)} />
-            )}
-            {!isLogginActive && (
-              <Register containerRef={ref => (this.current = ref)} />
-            )}
-          </div>
-          <RightSide
-            current={current}
-            currentActive={currentActive}
-            containerRef={ref => (this.rightSide = ref)}
-            onClick={this.changeState.bind(this)}
-          />
-        </div>
+      <div>
+        <GoogleLogin
+          clientId="438076043163-e4m9b8jp6rnrgb731a48k5jraggoqs3n.apps.googleusercontent.com"
+          buttonText="Login to ParkS"
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+          cookiePolicy={"single_host_origin"}
+        />
       </div>
     );
   }
 }
-
-const RightSide = props => {
-  return (
-    <div
-      className="right-side"
-      ref={props.containerRef}
-      onClick={props.onClick}
-    >
-      <div className="inner-container">
-        <div className="text">{props.current}</div>
-      </div>
-    </div>
-  );
-};
 
 export default App;
