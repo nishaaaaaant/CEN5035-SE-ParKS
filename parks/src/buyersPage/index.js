caa1b44b731d3f0ab66d3f5055359e0222039fd8
Suraj Mishra <suraj@Surajs-MacBook-Pro.local>
2022-02-04 17:35:43 -0500

    Added homepage component

---
+++ parks/src/buyersPage/index.js
@@ -0,0 +1,11 @@
+import React, { useEffect, lazy, Suspense } from "react";
+
+const BuyersPage = () => {
+  return (
+    <div>
+      <h3>Buyers Page</h3>
+    </div>
+  );
+};
+
+export default React.memo(BuyersPage);
--- parks/src/routes.js
+++ parks/src/routes.js
@@ -2,11 +2,15 @@ import React, { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 
 const Home = lazy(() => import("./home"));
+const BuyersPage = lazy(() => import("./buyersPage"));
+const RentersPage = lazy(() => import("./rentersPage"));
 
 const AppRoutes = () => (
   <Suspense fallback={""}>
     <Routes>
       <Route exact path="/" element={<Home/>}></Route>
+      <Route exact path="/buyer" element={<BuyersPage/>}></Route>
+      <Route exact path="/renter" element={<RentersPage/>}></Route>
     </Routes>
   </Suspense>
 );
