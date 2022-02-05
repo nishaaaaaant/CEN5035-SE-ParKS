3413cf1f9a14a3e2a7debb04ba7fd5c9f836c28a
Suraj Mishra <suraj@Surajs-MacBook-Pro.local>
2022-02-04 20:25:27 -0500

    Added Registraion API and Fetch all Users API

--- server/app.go
+++ server/app.go
@@ -1,68 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+	"github.com/gofiber/fiber/v2"
 
-	"github.com/gorilla/mux"
-	"gopkg.in/mgo.v2/bson"
+	"parks/m/v2/configs"
+	"parks/m/v2/routes"
 )
 
-type Movie struct {
-	ID          bson.ObjectId `bson:"_id" json:"id"`
-	Name        string        `bson:"name" json:"name"`
-	CoverImage  string        `bson:"cover_image" json:"cover_image"`
-	Description string        `bson:"description" json:"description"`
-}
-
-func AllMoviesEndPoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "neerav chutiyaa !")
-}
-
-func FindMovieEndpoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
-}
-
-func CreateMovieEndPoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
-}
-
-func UpdateMovieEndPoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
-}
-
-func DeleteMovieEndPoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
-}
-
-// func CreateMovieEndPoint(w http.ResponseWriter, r *http.Request) {
-// 	defer r.Body.Close()
-// 	var movie Movie
-// 	if err := json.NewDecoder(r.Body).Decode(&movie); err != nil {
-// 		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
-// 		return
-// 	}
-// 	movie.ID = bson.NewObjectId()
-// 	if err := dao.Insert(movie); err != nil {
-// 		respondWithError(w, http.StatusInternalServerError, err.Error())
-// 		return
-// 	}
-// 	respondWithJson(w, http.StatusCreated, movie)
-// }
-
-func respondWithError(w http.ResponseWriter, i int, s string) {
-	panic("unimplemented")
-}
-
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/movies", AllMoviesEndPoint).Methods("GET")
-	r.HandleFunc("/movies", CreateMovieEndPoint).Methods("POST")
-	r.HandleFunc("/movies", UpdateMovieEndPoint).Methods("PUT")
-	r.HandleFunc("/movies", DeleteMovieEndPoint).Methods("DELETE")
-	r.HandleFunc("/movies/{id}", FindMovieEndpoint).Methods("GET")
-	if err := http.ListenAndServe(":3000", r); err != nil {
-		log.Fatal(err)
-	}
+	app := fiber.New()
+
+	//run database
+	configs.ConnectDB()
+
+	routes.UserRoute(app)
+
+	app.Listen(":6000")
 }
