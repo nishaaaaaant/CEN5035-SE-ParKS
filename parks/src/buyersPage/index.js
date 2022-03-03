6efe194a1902d7f50dffe4a8e17c8e4e67595147
Suraj Mishra <suraj@Surajs-MacBook-Pro.local>
2022-03-03 18:04:53 -0500

    Initialized map api

--- parks/src/buyersPage/index.js
+++ parks/src/buyersPage/index.js
@@ -1,8 +1,12 @@
 import React, { lazy, Suspense } from "react";
+// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 
 const NavbarComponent = lazy(() => import("../common/navbar"));
 
 const BuyersPage = () => {
+  const position = [51.505, -0.09];
+
   const renderNavbar = () => {
     return (
       <Suspense fallback={""}>
@@ -11,10 +15,27 @@ const BuyersPage = () => {
     );
   };
 
+  const myMap = L.map("parksMap", {
+    center: [37.7749, -122.4194],
+    zoom: 13,
+  });
+
   return (
     <div>
       {renderNavbar()}
-      <h3 style={{ marginTop: 60 }}>Buyers Page</h3>
+      <div id="parksMap" style={{ width: "100vw", height: 500, marginTop: 70 }}>
+        {/* <MapContainer center={position} zoom={13}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer> */}
+      </div>
     </div>
   );
 };
