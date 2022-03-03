e0b18dff94127a1cbf36a5c1f2847c9d3726d04c
nishaaaaaant <nishantvishwanadha@gmail.com>
2022-03-03 17:06:10 -0500

    Added some changes to the index.html, added logo, and added content to about us.

--- parks/src/about/index.js
+++ parks/src/about/index.js
@@ -11,7 +11,16 @@ const About = () => {
     );
   };
 
-  return renderNavbar();
+  return (
+    <div>
+      {renderNavbar()}
+      <h3 style={{ marginTop: 60 }}>About Us</h3>
+      <p>We’re on a mission to empower people to get everywhere, easier!</p>
+      <p>By designing better ways to find, reserve and pay for parking, we make life easier, our cities more accessible, and our world more connected.</p>
+      <p>Reserve a space with a few taps and skip the parking hunt.</p>
+      <p>Park your car in seconds and go do your thing.</p>
+    </div>
+  );
 };
 
 export default React.memo(About);
