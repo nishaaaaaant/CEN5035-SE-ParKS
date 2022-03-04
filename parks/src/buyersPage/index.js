848ba3758d6252daa91034e68bc1020b55163a1f
nishaaaaaant <nishantvishwanadha@gmail.com>
2022-03-04 13:19:22 -0500

    Fixed the Maps api error

--- parks/src/buyersPage/index.js
+++ parks/src/buyersPage/index.js
@@ -4,7 +4,7 @@ import MyMap from "./MyMap";
 const NavbarComponent = lazy(() => import("../common/navbar"));
 
 const BuyersPage = () => {
-  const position = [51.505, -0.09];
+  // const position = [51.505, -0.09];
 
   const renderNavbar = () => {
     return (
@@ -14,10 +14,10 @@ const BuyersPage = () => {
     );
   };
 
-  const myMap = L.map("parksMap", {
-    center: [37.7749, -122.4194],
-    zoom: 13,
-  });
+  // const myMap = L.map("parksMap", {
+  //   center: [37.7749, -122.4194],
+  //   zoom: 13,
+  // });
 
   return (
     <div>
