a94f5c786801a4738b6382f39ea0831a90745f74
Suraj Mishra <smsuraj100@gmail.com>
2022-04-01 16:11:58 -0400

    buyers page and map implementation

--- parks/src/constants.js
+++ parks/src/constants.js
@@ -1,3 +1,5 @@
 // API URLs
 export const REGISTRATION_API = "http://127.0.0.1:8080/user";
 export const LOGIN_API = "http://127.0.0.1:8080/login";
+export const MAPBOX_TOKEN =
+  "pk.eyJ1Ijoic21zdXJhajEwMCIsImEiOiJjbDFncWZlbzQwNmtqM2lzOGMwdGZrb2hrIn0.nyjvR9DFIs7xwaiaaBHZ8w";
