d24b850c32b6452e94e33edad2fef0f11c2fb718
nishaaaaaant <nishantvishwanadha@gmail.com>
2022-04-01 18:21:05 -0400

    resolved conflict

--- parks/cypress/integration/AddressCheck.spec.js
+++ parks/cypress/integration/AddressCheck.spec.js
@@ -3,5 +3,9 @@ describe("check the location functionality of the maps integrated",() => {
     it("checking the location on maps",() => {
         cy.visit("/buyer")
         cy.get("#buyerPageDiv").should("exist")
+        /* ==== Generated with Cypress Studio ==== */
+        cy.get('.sc-dkPtRN > :nth-child(1) > button').click();
+        cy.get('.sc-furwcr').click();
+        /* ==== End Cypress Studio ==== */
     });
 });
--- parks/src/index.js
+++ parks/src/index.js
@@ -8,7 +8,6 @@ import "leaflet/dist/leaflet.css";
 import { Provider } from "react-redux";
 import store from "./store";
 
-
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
