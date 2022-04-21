ee3e30db81d0ca9b8d5ebbe7a313e7b1abef5c4f
Suraj Mishra <smsuraj100@gmail.com>
2022-04-20 23:54:39 -0400

    Deployed frontend on firebase

--- parks/src/App.js
+++ parks/src/App.js
@@ -1,6 +1,21 @@
 import React, { Component, lazy, Suspense } from "react";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import Routes from "./routes";
 const NavbarComponent = lazy(() => import("./common/navbar"));
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDPedji5Lfzoym3LZgzvyklzexn-cHSYGE",
+  authDomain: "parks-39379.firebaseapp.com",
+  projectId: "parks-39379",
+  storageBucket: "parks-39379.appspot.com",
+  messagingSenderId: "404987069554",
+  appId: "1:404987069554:web:5087840109ccca763ceeeb",
+  measurementId: "G-H9J5XYF17V",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export class App extends Component {
   renderNavbar = () => {
